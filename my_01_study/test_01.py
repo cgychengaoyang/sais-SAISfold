2d98600a8b2183292ce0882bb0997d836c6b7440
chengaoyang <chengaoyang_q@163.com>
2026-05-07 17:40:38 +0800

    ...

--- my_01_study/test_01.py
+++ my_01_study/test_01.py
@@ -1,32 +1,14 @@
 import os
-import torch
-print('PyTorch version:', torch.__version__)
-print('CUDA available:', torch.cuda.is_available())
-print('CUDA version:', torch.version.cuda)
-print('Device name:', torch.cuda.get_device_name(0))
 
+from my_01_study.utils.root_utils import root_path
 
-is_win = 1
-if os.name == 'nt':  # Windows系统
-    print('Windows系统')
-else:
-    is_win = 0
-    print('Linux系统')
-
-# /mnt/d/develop/PyCharm_workspaces/study_01/my_00_data  # 存储数据的根目录
-# /home/develop_workspaces/study_03/my_301_SAISfold  # 工作目录...
-# linux 直接使用绝对路径更简单
-root_path = '/mnt/d/develop/PyCharm_workspaces/study_01/my_00_data/sais/data_16/old_data'  # 存储数据的根目录
-
-if is_win == 1:
-    root_path = '../../../../study_01/my_00_data/sais/data_16/old_data'  # 存储数据的根目录
-
-FILE_PATH = f"{root_path}/Nucleic_protein_res4.0.zip"  # ...
+FILE_PATH = f"{root_path}/sais/data_16/old_data/Nucleic_protein_res4.0.zip"  # ...
 
 def main():
     print("=" * 60)
     print("  Nucleic_protein_res4.0.zip 数据集检测工具")
     print("=" * 60)
+    # /home/develop_workspaces/study_03/my_301_SAISfold  # 工作目录...
     # 打印【当前工作目录】（Python程序运行的目录）
     print("当前工作目录:", os.getcwd())
     # 打印【FILE_PATH的完整绝对路径】（文件实际路径）
---
+++ my_01_study/utils/root_utils.py
@@ -0,0 +1,22 @@
+import os
+import torch
+print('Device name:', torch.cuda.get_device_name(0))
+print('PyTorch version:', torch.__version__)
+print('CUDA available:', torch.cuda.is_available())
+print('CUDA version:', torch.version.cuda)
+
+
+is_win = 1
+if os.name == 'nt':  # Windows系统
+    print('Windows系统')
+else:
+    is_win = 0
+    print('Linux系统')
+
+# /mnt/d/develop/PyCharm_workspaces/study_01/my_00_data  # 存储数据的根目录
+# linux 直接使用绝对路径更简单
+root_path = '/mnt/d/develop/PyCharm_workspaces/study_01/my_00_data/sais/data_15/old_data'  # 存储数据的根目录
+
+if is_win == 1:
+    root_path = '../../../../study_01/my_00_data/sais/data_15/old_data'  # 存储数据的根目录
+
